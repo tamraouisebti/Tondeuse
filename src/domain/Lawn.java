32acb6fdf756b26e330f43c93dbb453955a68995
tamraouisebti <32700254+tamraouisebti@users.noreply.github.com>
2023-02-17 10:05:16 +0100

    create project

---
+++ src/domain/Lawn.java
@@ -0,0 +1,80 @@
+package domain;
+
+import exception.BadlyFormattedInputFileException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lawn {
+    private int xSize;
+    private int ySize;
+    private List<Mower> mowers;
+
+    public Lawn(String fileName) {
+        File file = new File(fileName);
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            initSize(bufferedReader);
+            initMowers(bufferedReader);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (BadlyFormattedInputFileException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private void initSize(BufferedReader bufferedReader) throws IOException, BadlyFormattedInputFileException {
+        String sizeString;
+        if ((sizeString = bufferedReader.readLine()) != null) {
+            var sizeArray = sizeString.split(" ");
+            if (sizeArray.length != 2)
+                throw new BadlyFormattedInputFileException();
+            this.xSize = Integer.parseInt(sizeArray[0]);
+            this.ySize = Integer.parseInt(sizeArray[0]);
+        }
+    }
+
+    private void initMowers(BufferedReader bufferedReader) throws IOException, BadlyFormattedInputFileException {
+        this.mowers = new ArrayList<>();
+        String stringPosition;
+        String directionList;
+        while (((stringPosition = bufferedReader.readLine()) != null)) {
+            var positionArray = stringPosition.split(" ");
+            if (positionArray.length != 3)
+                throw new BadlyFormattedInputFileException();
+            String[] directionArray = null;
+            if ((directionList = bufferedReader.readLine()) != null) {
+                directionArray = directionList.split("");
+            }
+
+            this.mowers.add(mapMower(positionArray, directionArray));
+        }
+    }
+
+    private Mower mapMower(String[] positionArray, String[] directionArray) {
+        Position position = new Position(Integer.parseInt(positionArray[0]), Integer.parseInt(positionArray[1]));
+        Orientation orientation = Orientation.valueOf(positionArray[2]);
+        var directionList = Arrays.stream(directionArray).map(Direction::valueOf).collect(Collectors.toList());
+
+        return new Mower(position, orientation, directionList);
+    }
+
+    public int getxSize() {
+        return xSize;
+    }
+
+    public int getySize() {
+        return ySize;
+    }
+
+    public List<Mower> getMowers() {
+        return mowers;
+    }
+}
