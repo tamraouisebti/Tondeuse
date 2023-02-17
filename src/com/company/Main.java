4c87b19ae3df125f25acfd8b44189e93242c158d
tamraouisebti <32700254+tamraouisebti@users.noreply.github.com>
2023-02-17 10:01:27 +0100

    create project

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import service.MowerService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MowerService.mow("src/testFile/test.txt");
+    }
+}
