0f57d0c22f01e76d6d81df90038499f4b4babf96
tamraouisebti <32700254+tamraouisebti@users.noreply.github.com>
2023-02-18 16:56:53 +0100

    Optional input for the file, in case no input use the test file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ import service.MowerService;
 public class Main {
 
     public static void main(String[] args) {
-        MowerService.mow("src/testFile/test.txt");
+        // if the file name is not given then use the test file name
+        if (args.length != 1 || args[0] == null || args[0].isEmpty())
+            MowerService.mow("src/testFile/test.txt");
+        else MowerService.mow(args[0]);
     }
 }
